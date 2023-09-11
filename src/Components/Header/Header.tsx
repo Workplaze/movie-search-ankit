fe1fca4c889c0897768e795866a4e076ecb6179f
ANKIT PANDEY <ankitpandey2223@gmail.com>
2023-09-11 05:57:43 -0700

    first commit

---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Header = () => {
+  return <div>
+    <div>
+      <h1>YTS</h1>
+    </div>
+    <div>
+      <nav>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/">3D</a></li>
+          <li><a href="/">4K</a></li>
+          <li><a href="/">Trending</a></li>
+          <li><a href="/">Browse Movies</a></li>
+        </ul>
+      </nav>
+    </div>
+
+
+  </div>;
+};
+
+export default Header;
