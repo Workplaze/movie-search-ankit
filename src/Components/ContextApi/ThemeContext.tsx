c0bc829685ec33428913b457584db49475c4828c
ANKIT PANDEY <ankitpandey2223@gmail.com>
2023-09-12 16:34:24 +0530

    [ADD] context api

---
+++ src/Components/ContextApi/ThemeContext.tsx
@@ -0,0 +1,34 @@
+import React, { createContext, useContext, useState, ReactNode } from 'react';
+
+interface ThemeContextType {
+  isDarkMode: boolean;
+  toggleTheme: () => void;
+}
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+export const useTheme = () => {
+  const context = useContext(ThemeContext);
+  if (!context) {
+    throw new Error('useTheme must be used within a ThemeProvider');
+  }
+  return context;
+}
+
+interface ThemeProviderProps {
+  children: ReactNode;
+}
+
+export function ThemeProvider({ children }: ThemeProviderProps) {
+  const [isDarkMode, setIsDarkMode] = useState<boolean>(false);
+
+  const toggleTheme = () => {
+    setIsDarkMode((prev) => !prev);
+  };
+
+  return (
+    <ThemeContext.Provider value={{ isDarkMode, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import "./style.css";
+import { useTheme } from "../ContextApi/ThemeContext";
 
 const Header = () => {
+  const {isDarkMode,toggleTheme} =useTheme();
   return (
     <div className="header">
       <div className="nav">
         <div>
           <h1>Yts</h1>
+        </div>
+        <div className="button">
+          <button onClick={toggleTheme}>
+            {isDarkMode? 'Light Mode': 'Dark Mode' }
+          </button>
         </div>
         <div>
           
