95978a96abb29cc96101fd4dd67cf963e8f31ea0
ANKIT PANDEY <ankitpandey2223@gmail.com>
2023-09-21 13:33:35 +0530

    [FIX] made queries to single component

---
+++ src/Components/Apollo/Query/Queries.tsx
@@ -0,0 +1,26 @@
+import { gql } from "@apollo/client";
+
+export const GET_USERDATA = gql`
+  query MyQuery($role: String!) {
+    user(where: { role: { _eq: $role } }) {
+      id
+      address
+      dob
+      email_id
+      first_name
+      gender
+      last_name
+      mobile_number
+      role
+      status
+    }
+  }
+`;
+
+export const GET_FILTER_OPTIONS = gql`
+  query FilterOptions {
+    distinct_roles: user(distinct_on: role) {
+      role
+    }
+  }
+`;
